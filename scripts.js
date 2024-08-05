5eeee52d5c79d622771647a93d6bab72fa5b3620
Kaiden Bell <144740949+Kaiden-Bell@users.noreply.github.com>
2024-08-05 18:35:05 +0000

    Updated Again 8/5/24

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const businessName = "BELL OF THE BALL";
     let i = 0;
-    const typingSpeed = 100;
+    const typingSpeed = 52;
   
     function typeWriter() {
       if (i < businessName.length) {
